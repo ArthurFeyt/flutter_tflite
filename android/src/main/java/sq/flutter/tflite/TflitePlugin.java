12c4726705865ea1236caa4ed4f905c8110b60a6
arthur.feyt <arthur.feyt@gmail.com>
2025-05-25 16:38:06 +0200

    likugj

--- android/src/main/java/sq/flutter/tflite/TflitePlugin.java
+++ android/src/main/java/sq/flutter/tflite/TflitePlugin.java
@@ -662,9 +662,9 @@ public class TflitePlugin implements MethodCallHandler {
       this.outputScores = new float[1][num];
       this.inputArray = new Object[]{imgData};
 
-      outputMap.put(2, numDetections);
-      outputMap.put(0, outputClasses);
+      outputMap.put(0, numDetections);
       outputMap.put(1, outputScores);
+      outputMap.put(2, outputClasses);
       outputMap.put(3, outputLocations);
 
       startTime = SystemClock.uptimeMillis();
